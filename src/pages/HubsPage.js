117db223d7ce8820bbfa6e6f3cd3b50fed92f46b
Tayebwa-ian <tayebwaian0@gmail.com>
2024-08-21 18:07:13 +0200

    add more CRUD hooks

--- src/pages/HubsPage.js
+++ src/pages/HubsPage.js
@@ -1,7 +1,12 @@
 import { Container, Grid, Typography } from "@mui/material";
 import HorizontalHubCard from "../components/HorizontalHubCard";
+import useGet from "../custom_hooks/useGet";
 
 export default function HubsPage() {
+    const {data: hubs, isPending, error} = useGet("http://127.0.0.1:5000/api/core/hubs");
+    console.log(hubs);
+    console.log(isPending);
+    console.log(error);
     return (
         <Container
         sx={{
